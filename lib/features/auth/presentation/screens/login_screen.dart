5e39959c5ef8b8939271555221c065c22893ea5c
rasheduzzamanrochi <rasheduzzaman2003@gmail.com>
2026-04-26 15:26:15 +0600

    Remove unused screens and clean up imports in navigation and authentication files

--- lib/features/auth/presentation/screens/login_screen.dart
+++ lib/features/auth/presentation/screens/login_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
 import '../../../../core/constants.dart';
 import '../../provider/auth_provider.dart';
 import '../widgets/auth_shell.dart';
